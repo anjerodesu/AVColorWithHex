09513b5f6fa135675f1b0abbee45f359601b62cb
Yusuke Sakurai <kerokerokerop@gmail.com>
2014-02-27 19:12:13 +0900

    fix coloWithHexString method
    - doesn't recognize '0' starting string

--- ColorWithHexTests/ColorWithHexTests.m
+++ ColorWithHexTests/ColorWithHexTests.m
@@ -7,7 +7,7 @@
 //
 
 #import "ColorWithHexTests.h"
-
+#import "AVHexColor.h"
 
 @implementation ColorWithHexTests
 
@@ -25,9 +25,19 @@
     [super tearDown];
 }
 
+#define TEST_COLOR(hex) c = [AVHexColor colorWithHexString:hex]; STAssertNotNil(c,@"" ); NSLog(@"%@",c);
 - (void)testExample
 {
-    STFail(@"Unit tests are not implemented yet in ColorWithHexTests");
+    AVColor *c;
+    // rgb
+    TEST_COLOR(@"#fff");
+    TEST_COLOR(@"000");
+    TEST_COLOR(@"ff0000");
+    TEST_COLOR(@"#0000ff");
+    TEST_COLOR(@"#ee8899");
+    /// rgba
+    TEST_COLOR(@"#ff00ffff")
+    TEST_COLOR(@"00000000")
 }
 
 @end
